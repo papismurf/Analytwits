7a40656878e15a0c21e5e5b7df2a4fcb8ce177cc
papismurf <33459750+papismurf@users.noreply.github.com>
2018-05-28 17:58:13 +0100

    Added polarity_score sentiment analysis loop for tweets in trends

--- moonshot/tweet_script.py
+++ moonshot/tweet_script.py
@@ -7,10 +7,10 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import pandas as pd
 import matplotlib.pyplot as plt
 
-ACCESS_TOKEN = 'ACCESS_TOKEN'
-ACCESS_TOKEN_SECRET = 'ACCESS_TOKEN_SECRET'
-CONSUMER_KEY = 'CONSUMER_KEY'
-CONSUMER_SECRET = 'CONSUMER_SECRET'
+ACCESS_TOKEN = '276176566-rCDQiAgggd4cIqeiHIN8Z18iHMgXyEnH5ZbDIf1R'
+ACCESS_TOKEN_SECRET = 'v4f3Ql6QnBNmLz6acDZPmkGhiXsuMJRyZWAkcalesIb2w'
+CONSUMER_KEY = 'nJIXAllPVZGtKpQJmnH2q8YbC'
+CONSUMER_SECRET = 'O6YzlUQwUOaKEMhBFo6whuULxbtfmLDPCImSTGSmduWD9a2XFK'
 
 # Define the location id for the UK
 # With Yahoo Where on Earth ID
@@ -20,7 +20,7 @@ LANG = "en"
 # Define type of tweets we are after
 TWEETS_TYPE = "recent"
 # Define max number of tweets per trend
-MAX_STATUSES = 1000
+MAX_STATUSES = 10
 
 # API configuration
 # API with request rate limited
@@ -52,6 +52,13 @@ for trend in uk_trends:
 
 # Init sentiment analysis object
 analyzer = SentimentIntensityAnalyzer()
+# Loop through tweets and analyse sentiment polarity
+for result in search_results:
+    result = list(str(result.text))
+    statuses = result[0][3]
+    compoundSentiment = analyzer.polarity_scores(statuses)
+    print(compoundSentiment)
+
 # Compound sentiment extraction use example
 # exampleCompoundSentiment = analyzer.polarity_scores("I love cats and dogs")["compound"]
 # print(exampleCompoundSentiment)
